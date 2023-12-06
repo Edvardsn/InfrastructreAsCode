903a3a21f1ec55320b9f9a5d0cc771d8457b128a
Edvardsn <petter-edvardsen@hotmail.com>
2023-12-06 21:45:08 +0100

    Removal of headline.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>This is built with Continous deployment<h1>"
+    return "This is built with Continous deployment"
 
 
 print("Webserver listening on port: ", os.environ["WEBSERVER_PORT"])
