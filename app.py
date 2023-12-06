70b21693d480998c5da768eb3bb2366018c95a88
Edvardsn <petter-edvardsen@hotmail.com>
2023-12-06 23:19:39 +0100

    Tests fix.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "This is a built with a Continous Deployment pipeline!"
+    return "This is built with a Continous Deployment pipeline!"
 
 
 @app.route("/")
