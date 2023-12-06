8f25c7b09c8ec1a66e2f2522013178b091910297
Edvardsn <petter-edvardsen@hotmail.com>
2023-12-06 21:41:34 +0100

    New version with headline

--- app.py
+++ app.py
@@ -5,65 +5,12 @@ import os
 
 app = Flask(__name__)
 
-# # Varaibles for the database connection used for Development
-# db_name = os.environ["DB_NAME"]
-# db_host = os.environ["DB_HOST"]
-# db_port = os.environ["DB_PORT"]
-# db_user = os.environ["DB_USER"]
-# db_password = os.environ["DB_PASSWORD"]
-
 
 @app.route("/")
 def hello_world():
-    return "This is finally working!"
+    return "<h1>This is built with Continous deployment<h1>"
 
 
-# @app.route("/widgets")
-# def get_widgets():
-#     with psycopg2.connect(
-#         host=db_host,
-#         user=db_user,
-#         password=db_password,
-#         database=db_name,
-#         port=db_port,
-#     ) as conn:
-#         with conn.cursor() as cur:
-#             cur.execute("SELECT * FROM widgets")
-#             row_headers = [x[0] for x in cur.description]
-#             results = cur.fetchall()
-#     conn.close()
-
-#     json_data = [dict(zip(row_headers, result)) for result in results]
-#     return json.dumps(json_data)
-
-
-# @app.route("/initdb")
-# def db_init():
-#     conn = psycopg2.connect(
-#         host=db_host, user=db_user, password=db_password, database=db_name, port=db_port
-#     )
-#     conn.set_session(autocommit=True)
-#     with conn.cursor() as cur:
-#         cur.execute("DROP DATABASE IF EXISTS example")
-#         cur.execute("CREATE DATABASE example")
-#     conn.close()
-
-#     with psycopg2.connect(
-#         host=db_host, user=db_user, password=db_password, database=db_name, port=db_port
-#     ) as conn:
-#         with conn.cursor() as cur:
-#             cur.execute("DROP TABLE IF EXISTS widgets")
-#             cur.execute(
-#                 "CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))"
-#             )
-#     conn.close()
-
-#     return "init database"
-
-
-# # Debugging info
-# print("Database properties: ", db_name, db_host, db_port)
-# print("-----------------------------------")
 print("Webserver listening on port: ", os.environ["WEBSERVER_PORT"])
 print("-----------------------------------")
 
