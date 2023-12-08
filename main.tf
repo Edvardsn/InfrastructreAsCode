be1dfeabf825f92077528fa5f207f83b9124b6fd
Edvardsn <petter-edvardsen@hotmail.com>
2023-12-08 10:54:26 +0100

    Project report.

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "google_compute_instance" "skytjenester_vm" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+      image = "ubuntu-os-cloud/ubuntu-minimal-latest"
     }
   }
   # Install Flask
